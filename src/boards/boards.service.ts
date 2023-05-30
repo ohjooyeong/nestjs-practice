5fa97df9d4dc9aefa1a08b749c62d3a32ea03298
OH-JOO-YEONG <brb1111@naver.com>
2023-05-30 20:27:20 +0900

    [Feat] Board Model 정의 게시글 생성 Service, Controller 부분

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { Board, BoardStatus } from './board.model';
+import { v1 as uuid } from 'uuid';
 
 @Injectable()
-export class BoardsService {}
+export class BoardsService {
+  private boards: Board[] = []; // private 한 이유 - 다른 컴포넌트에서 boards를 수정할 수 없게 막도록 함.
+
+  getAllBoards(): Board[] {
+    return this.boards;
+  }
+
+  createBoard(title: string, description: string) {
+    const board: Board = {
+      id: uuid(),
+      title,
+      description,
+      status: BoardStatus.PUBLIC,
+    };
+
+    this.boards.push(board);
+    return board;
+  }
+}

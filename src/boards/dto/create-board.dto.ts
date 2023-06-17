8b72ca64eea3a9671cdf741b6760746aa43abd69
OH-JOO-YEONG <brb1111@naver.com>
2023-06-18 01:54:03 +0900

    [Feat] Pipe를 이용한 Validation 체크

--- src/boards/dto/create-board.dto.ts
+++ src/boards/dto/create-board.dto.ts
@@ -1,6 +1,12 @@
 // 클래스는 인터페이스와 다르게 런타임에서 작동하기 때문에
+
+import { IsNotEmpty } from 'class-validator';
+
 // 파이프 같은 기능을 이용할 때 더 유용합니다.
 export class CreateBoardDto {
+  @IsNotEmpty()
   title: string;
+
+  @IsNotEmpty()
   description: string;
 }
